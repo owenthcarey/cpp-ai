dc03f67c80c322fa287b2cf83cfbda67790c9b39
owenthcarey <owenthcarey@gmail.com>
2023-05-28 18:55:20 -0700

    Initial commit

---
+++ LinearRegression.cpp
@@ -0,0 +1,5 @@
+//
+// Created by owenthcarey on 5/28/23.
+//
+
+#include "LinearRegression.h"
---
+++ LinearRegression.h
@@ -0,0 +1,14 @@
+//
+// Created by owenthcarey on 5/28/23.
+//
+
+#ifndef CPP_AI_LINEARREGRESSION_H
+#define CPP_AI_LINEARREGRESSION_H
+
+
+class LinearRegression {
+
+};
+
+
+#endif //CPP_AI_LINEARREGRESSION_H
