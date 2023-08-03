32509edc868db1ae34798f259273c06c2eb3aed3
owenthcarey <owenthcarey@gmail.com>
2023-08-02 20:11:22 -0700

    Add logistic regression

---
+++ ml/include/LogisticRegression.h
@@ -0,0 +1,14 @@
+//
+// Created by Owen Carey on 8/2/23.
+//
+
+#ifndef CPP_AI_LOGISTICREGRESSION_H
+#define CPP_AI_LOGISTICREGRESSION_H
+
+
+class LogisticRegression {
+
+};
+
+
+#endif //CPP_AI_LOGISTICREGRESSION_H
---
+++ ml/src/LogisticRegression.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Owen Carey on 8/2/23.
+//
+
+#include "../include/LogisticRegression.h"
