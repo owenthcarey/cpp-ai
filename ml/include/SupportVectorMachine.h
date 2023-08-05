c4ba789b6c01d439441982098a90b209faf8e02b
owenthcarey <owenthcarey@gmail.com>
2023-08-04 21:39:49 -0700

    Add support vector machine

---
+++ ml/include/SupportVectorMachine.h
@@ -0,0 +1,14 @@
+//
+// Created by Owen Carey on 8/4/23.
+//
+
+#ifndef CPP_AI_SUPPORTVECTORMACHINE_H
+#define CPP_AI_SUPPORTVECTORMACHINE_H
+
+
+class SupportVectorMachine {
+
+};
+
+
+#endif //CPP_AI_SUPPORTVECTORMACHINE_H
---
+++ ml/src/SupportVectorMachine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Owen Carey on 8/4/23.
+//
+
+#include "../include/SupportVectorMachine.h"
