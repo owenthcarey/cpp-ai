957b1aab1b97bb8f6cce968f7195053e3951c250
owenthcarey <owenthcarey@gmail.com>
2023-08-09 09:29:53 -0700

    Add neural network

---
+++ dl/include/NeuralNetwork.h
@@ -0,0 +1,14 @@
+//
+// Created by Owen Carey on 8/9/23.
+//
+
+#ifndef CPP_AI_NEURALNETWORK_H
+#define CPP_AI_NEURALNETWORK_H
+
+
+class NeuralNetwork {
+
+};
+
+
+#endif //CPP_AI_NEURALNETWORK_H
---
+++ dl/src/NeuralNetwork.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Owen Carey on 8/9/23.
+//
+
+#include "../include/NeuralNetwork.h"
