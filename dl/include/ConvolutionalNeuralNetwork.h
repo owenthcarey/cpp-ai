2971187ed41590f56a1facf741358a21eab6bd39
owenthcarey <owenthcarey@gmail.com>
2023-08-10 15:51:08 -0700

    Add convolutional neural network

---
+++ dl/include/ConvolutionalNeuralNetwork.h
@@ -0,0 +1,14 @@
+//
+// Created by Owen Carey on 8/10/23.
+//
+
+#ifndef CPP_AI_CONVOLUTIONALNEURALNETWORK_H
+#define CPP_AI_CONVOLUTIONALNEURALNETWORK_H
+
+
+class ConvolutionalNeuralNetwork {
+
+};
+
+
+#endif //CPP_AI_CONVOLUTIONALNEURALNETWORK_H
---
+++ dl/src/ConvolutionalNeuralNetwork.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Owen Carey on 8/10/23.
+//
+
+#include "../include/ConvolutionalNeuralNetwork.h"
