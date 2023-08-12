8f4c19c51767ca27c4e7544ba49fe83205096ab1
owenthcarey <owenthcarey@gmail.com>
2023-08-11 21:13:14 -0700

    Add recurrent neural network

---
+++ dl/include/RecurrentNeuralNetwork.h
@@ -0,0 +1,14 @@
+//
+// Created by Owen Carey on 8/11/23.
+//
+
+#ifndef CPP_AI_RECURRENTNEURALNETWORK_H
+#define CPP_AI_RECURRENTNEURALNETWORK_H
+
+
+class RecurrentNeuralNetwork {
+
+};
+
+
+#endif //CPP_AI_RECURRENTNEURALNETWORK_H
---
+++ dl/src/RecurrentNeuralNetwork.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Owen Carey on 8/11/23.
+//
+
+#include "../include/RecurrentNeuralNetwork.h"
