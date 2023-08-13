4b4ab72deb9d3963fc14aa6fa608b2c325cf5f83
owenthcarey <owenthcarey@gmail.com>
2023-08-12 23:56:10 -0700

    Add generative adversarial network

---
+++ dl/include/GenerativeAdversarialNetwork.h
@@ -0,0 +1,14 @@
+//
+// Created by Owen Carey on 8/12/23.
+//
+
+#ifndef CPP_AI_GENERATIVEADVERSARIALNETWORK_H
+#define CPP_AI_GENERATIVEADVERSARIALNETWORK_H
+
+
+class GenerativeAdversarialNetwork {
+
+};
+
+
+#endif //CPP_AI_GENERATIVEADVERSARIALNETWORK_H
---
+++ dl/src/GenerativeAdversarialNetwork.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Owen Carey on 8/12/23.
+//
+
+#include "../include/GenerativeAdversarialNetwork.h"
