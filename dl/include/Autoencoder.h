0d0505cd4502bd8e1e94f6381c69c8b9ae51203c
owenthcarey <owenthcarey@gmail.com>
2023-08-13 23:13:01 -0700

    Add autoencoder

---
+++ dl/include/Autoencoder.h
@@ -0,0 +1,14 @@
+//
+// Created by Owen Carey on 8/13/23.
+//
+
+#ifndef CPP_AI_AUTOENCODER_H
+#define CPP_AI_AUTOENCODER_H
+
+
+class Autoencoder {
+
+};
+
+
+#endif //CPP_AI_AUTOENCODER_H
---
+++ dl/src/Autoencoder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Owen Carey on 8/13/23.
+//
+
+#include "../include/Autoencoder.h"
